6b64075b69a810a7b62ca79f575e76098a676731
rahimm <rahimov.muhammadali@msg.grou>
2025-06-03 16:26:15 +0200

    updated version

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,11 +1,84 @@
 import { Component } from '@angular/core';
+import {NgClass, NgForOf, NgIf} from '@angular/common';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
-  imports: [],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.css'
+  standalone: true,
+  imports: [
+    NgClass,
+    FormsModule,
+    NgIf,
+    NgForOf
+  ],
+  styleUrls: ['./contact.component.css']
 })
 export class ContactComponent {
+  currentStep = 1;
+  steps = [1, 2, 3, 4];
 
+
+  formData = {
+    name: '',
+    email: '',
+    phone: '',
+    message: ''
+  };
+
+  get charCount(): number {
+    return this.formData.message.length;
+  }
+
+  nextStep() {
+    if (this.currentStep < 4) {
+      this.currentStep++;
+    }
+  }
+
+  prevStep() {
+    if (this.currentStep > 1) {
+      this.currentStep--;
+    }
+  }
+
+  submitForm() {
+    const formspreeURL = 'https://formspree.io/f/xldbaeep'; // Ersetze 'yourFormId'
+
+    const formData = {
+      name: this.formData.name,
+      email: this.formData.email,
+      phone: this.formData.phone,
+      message: this.formData.message
+    };
+
+    fetch(formspreeURL, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+      .then(res => res.json())
+      .then(response => {
+        if (response.ok || response.success !== false) {
+          alert('Nachricht erfolgreich gesendet!');
+          this.currentStep = 1;
+          this.formData = { name: '', email: '', phone: '', message: '' };
+        } else {
+          alert('Fehler beim Senden: ' + (response.error || 'Unbekannter Fehler'));
+        }
+      })
+      .catch(error => {
+        console.error('Fehler:', error);
+        alert('Fehler beim Senden.');
+      });
+  }
+
+
+
+  isStepActive(step: number): boolean {
+    return this.currentStep === step;
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component} from '@angular/core';
-import {RouterLink} from '@angular/router';
+import {TranslatePipe} from '@ngx-translate/core';
 
 
 @Component({
   selector: 'app-home',
   imports: [
-    RouterLink
+    TranslatePipe
   ],
   templateUrl: './home.component.html',
   standalone: true,
