26530dddf3fb31c316bb7764c9b465e7761b1e5f
rahimm <muhammadali.rahimov@msg.group>
2025-08-28 12:57:09 +0200

    added some animations

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
-import { Component} from '@angular/core';
-import {TranslatePipe} from '@ngx-translate/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslatePipe } from '@ngx-translate/core';
 
 
 @Component({
@@ -11,6 +11,33 @@ import {TranslatePipe} from '@ngx-translate/core';
   standalone: true,
   styleUrl: './home.component.css'
 })
-export class HomeComponent{
+export class HomeComponent implements OnInit {
+  yearsCount = 0;
+  projectsCount = 0;
+  deliveryCount = 0;
 
+  private yearsTarget = 894;
+  private projectsTarget = 8;
+  private deliveryTarget = 98;
+
+  ngOnInit(): void {
+    this.animateCount('yearsCount', this.yearsTarget, 10);
+    this.animateCount('projectsCount', this.projectsTarget, 1800);
+    this.animateCount('deliveryCount', this.deliveryTarget, 1800);
+  }
+
+  private animateCount(property: 'yearsCount' | 'projectsCount' | 'deliveryCount', target: number, duration: number) {
+    const stepTime = Math.max(Math.floor(duration / target), 10);
+    let current = 0;
+    const step = () => {
+      if (current < target) {
+        current++;
+        this[property] = current;
+        setTimeout(step, stepTime);
+      } else {
+        this[property] = target;
+      }
+    };
+    step();
+  }
 }
